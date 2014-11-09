ab00b064a136ab9e869c3c71573b90a5dc985940
eunjiseo <eunjiseo@yahoo.com>
2014-11-09 09:33:43 -0600

    Transfer page

---
+++ LonghornBank/LonghornBank/Transfer.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Transfer.aspx.vb" Inherits="SendEmail.Transfer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        From:
+        <asp:DropDownList ID="ddlTransferFrom" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        To:<br />
+        <asp:DropDownList ID="ddlTransferTo" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        Amount:<br />
+        <asp:TextBox ID="txtTransferAmount" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Transfer Date:<br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        [e.g. 10/24/14]<br />
+        <br />
+        <asp:Button ID="btnMakeTranfer" runat="server" Text="Transfer" OnClientClick="return confirm('Are you sure you want to submit ?')"/>
+        <br />
+        </div>
+    </form>
+            
+</body>
+
+</html>
---
+++ LonghornBank/LonghornBank/Transfer.aspx.vb
@@ -0,0 +1,39 @@
+﻿Public Class Transfer
+    Inherits System.Web.UI.Page
+
+    ' Dim DB As New DBCustomers
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        '    DB.GetAllAccountsSP()
+
+        'If IsPostBack = False Then
+
+        '    'fill ddl
+        '    ddlTransferFrom.DataSource = DB.CustDataset
+        '    ddlTransferFrom.DataTextField = "AccountName, AcountNumber, AccountBalance"
+        '    ddlTransferFrom.DataValueField = "Account"
+        '    ddlTransferFrom.DataBind()
+
+        '    ddlTransferTo.DataSource = DB.CustDataset
+        '    ddlTransferTo.DataTextField = "AccountName, AcountNumber, AccountBalance"
+        '    ddlTransferTo.DataValueField = "Account"
+        '    ddlTransferTo.DataBind()
+
+        'End If
+
+    End Sub
+
+    Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlTransferFrom.SelectedIndexChanged
+
+    End Sub
+
+    Protected Sub btnMakeTranfer_Click(sender As Object, e As EventArgs) Handles btnMakeTranfer.Click
+        'make sure there is enough money in account to transfer
+
+        If ddlTransferFrom.SelectedIndex = ddlTransferTo.SelectedIndex Then
+            MsgBox("Transfer successful")
+        End If
+
+    End Sub
+End Class
