b1a779efa24b3a69599da1a44b19fd0a6337b41a
eunjiseo <eunjiseo@yahoo.com>
2014-11-16 20:35:21 -0600

    Create ClassTransactionsDB

--- LonghornBank/LonghornBank/ClassCustomerDB.vb
+++ LonghornBank/LonghornBank/ClassCustomerDB.vb
@@ -154,30 +154,6 @@ Public Class ClassCustomerDB
         RunProcedureGetAllCustomers("usp_customers_get_all")
     End Sub
 
-    Public Sub RunProcedureGetAllTransactions(ByVal strAccountNumber As String)
-        'author: Katie Seo
-
-        ' CREATES INSTANCES OF THE CONNECTION AND COMMAND OBJECT
-        Dim objConnection As New SqlConnection(mstrConnection)
-        ' Tell SQL server the name of the stored procedure you will be executing
-        Dim mdbDataAdapter As New SqlDataAdapter(strAccountNumber, objConnection)
-        Try
-            ' SETS THE COMMAND TYPE TO "STORED PROCEDURE"
-            mdbDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure
-            ' clear dataset
-            Me.mDatasetCustomer.Clear()
-            ' OPEN CONNECTION AND FILL DATASET
-            mdbDataAdapter.Fill(mDatasetCustomer, "tblTransactions")
-            ' copy dataset to dataview
-            mMyView.Table = mDatasetCustomer.Tables("tblTransactions")
-        Catch ex As Exception
-            Throw New Exception("stored procedure is " & strAccountNumber.ToString & " error is " & ex.Message)
-        End Try
-    End Sub
-
-    Public Sub GetAllTransactions()
-
-    End Sub
 
     Public Sub RunSPwithOneParam(ByVal strSPName As String, ByVal strParamName As String, ByVal strParamValue As String)
         ' purpose to run a stored procedure with one parameter
---
+++ LonghornBank/LonghornBank/ClassTransactionsDB.vb
@@ -0,0 +1,22 @@
+﻿Public Class ClassTransactionsDB
+    Public Sub RunProcedureGetAllTransactions(ByVal strAccountNumber As String)
+        'author: Katie Seo
+
+        ' CREATES INSTANCES OF THE CONNECTION AND COMMAND OBJECT
+        Dim objConnection As New SqlConnection(mstrConnection)
+        ' Tell SQL server the name of the stored procedure you will be executing
+        Dim mdbDataAdapter As New SqlDataAdapter(strAccountNumber, objConnection)
+        Try
+            ' SETS THE COMMAND TYPE TO "STORED PROCEDURE"
+            mdbDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure
+            ' clear dataset
+            Me.mDatasetCustomer.Clear()
+            ' OPEN CONNECTION AND FILL DATASET
+            mdbDataAdapter.Fill(mDatasetCustomer, "tblTransactions")
+            ' copy dataset to dataview
+            mMyView.Table = mDatasetCustomer.Tables("tblTransactions")
+        Catch ex As Exception
+            Throw New Exception("stored procedure is " & strAccountNumber.ToString & " error is " & ex.Message)
+        End Try
+    End Sub
+End Class
